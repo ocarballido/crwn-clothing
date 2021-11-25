20bad565a96832daeb43bb1857709e0eebcfa977
ocarballido <ocarballido@gmail.com>
2021-11-25 09:28:59 +0100

    update stripe integration and favico

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors';
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component';
 
 const CheckoutPage = ({ total, cartItems}) => {
     return ( 
@@ -31,6 +32,12 @@ const CheckoutPage = ({ total, cartItems}) => {
             <div className="total">
                 <span>TOTAL: ${total}</span>
             </div>
+            <div className="test-warning">
+                *PLease use the following test credit card for payments
+                <br />
+                4242 4242 4242 4242 - Exp: 01/22 - CVVV: 123
+            </div>
+            <StripeCheckoutButton price={total} />
         </div>
     );
 };
