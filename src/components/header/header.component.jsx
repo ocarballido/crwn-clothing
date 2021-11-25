cb6db6ad0ef4ca542318da859417540e3a744ae9
ocarballido <ocarballido@gmail.com>
2021-11-25 16:27:27 +0100

    converting to styled component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './header.styles.scss';
+import { HeaderContainer, LogoContainer, OptionsContainer, OptionDiv, OptionLink } from './header.styles';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
@@ -12,24 +12,24 @@ import { selectCurrentUser } from '../../redux/user/user.selectors';
 
 const Header = ({ currentUser, hidden }) => {
     return ( 
-        <div className="header">
-            <Link className="logo-container" to="/">
+        <HeaderContainer>
+            <LogoContainer to="/">
                 <Logo className="logo" />
-            </Link>
-            <div className="options">
-                <Link className="option" to="/shop">SHOP</Link>
-                <Link className="option" to="/shop">CONTACT</Link>
+            </LogoContainer>
+            <OptionsContainer>
+                <OptionLink to="/shop">SHOP</OptionLink>
+                <OptionLink to="/shop">CONTACT</OptionLink>
                 {
                     currentUser
                     ?
-                    <div className="option" onClick={() => auth.signOut()}> SIGN OUT</div>
+                    <OptionLink as="div" onClick={() => auth.signOut()}> SIGN OUT</OptionLink>
                     :
-                    <Link className="option" to="/signin">SIGN IN</Link>
+                    <OptionLink to="/signin">SIGN IN</OptionLink>
                 }
                 <CartIcon />
-            </div>
+            </OptionsContainer>
             { hidden ? null : <CartDropdown /> }
-        </div>
+        </HeaderContainer>
     );
 };
 
