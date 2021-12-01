9eda9d0da6a4b01e15d938d190640db59455a793
ocarballido <ocarballido@gmail.com>
2021-12-01 16:44:19 +0100

    implement code that pulls in our suhop data from firestore and store iot in our reducer sjhop page, also included spinner

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,15 +1,46 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
+import { connect } from 'react-redux';
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component'
 import CollectionPage from '../collection/collection.component';
+import { firestore, convertCollectionSnapshotToMap } from '../../firebase/firebase.utils'
 
-const ShopPage = ({ match }) => {
-    return (
-		<div className="shop-page">
-			<Route exact path={`${match.path}`} component={CollectionsOverview} />
-			<Route path={`${match.path}/:collectionId`} component={CollectionPage} />
-		</div>
-	);
+import { updatedCollections } from '../../redux/shop/shop.actions';
+import WithSpinner from '../../components/with-spinner/with-spinner.component';
+
+const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview);
+const CollectionPagewWithSpinner = WithSpinner(CollectionsOverview);
+
+class ShopPage extends React.Component {
+	state = {
+		loadding: true
+	};
+	unsubscribeFromSnapshot = null;
+
+	componentDidMount() {
+		const { updatedCollections } = this.props;
+		const collectionRef = firestore.collection('collections');
+		collectionRef.get().then(snapshot => {
+			const collectionsMap = convertCollectionSnapshotToMap(snapshot);
+			updatedCollections(collectionsMap);
+			this.setState({loadding: false});
+		});
+	}
+
+	render() {
+		const { match } = this.props;
+		const { loadding } = this.state;
+		return (
+			<div className="shop-page">
+				<Route exact path={`${match.path}`} render={(props) =>  <CollectionsOverviewWithSpinner isLoading={loadding} {...props} />} />
+				<Route path={`${match.path}/:collectionId`} render={(props) =>  <CollectionPagewWithSpinner isLoading={loadding} {...props} />} />
+			</div>
+		);
+	}
 };
 
-export default ShopPage;
+const mapDispatchToProps = dispatch => ({
+	updatedCollections: collectionsMap => dispatch(updatedCollections(collectionsMap))
+});
+
+export default connect(null, mapDispatchToProps)(ShopPage);
